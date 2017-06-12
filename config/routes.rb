ff0e77c9a5f09c475380f033001c99f7755cff49
Chris Burnett <dameramu@gmail.com>
2017-06-12 16:40:55 +0100

    Routes to match datamodel

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :jobs do
+  namespace :api, defaults: { format: :json } do
+
+    # jobs/assignments
+    resources :jobs do
+      resources :assignments
+      resources :comments
+    end
+
+    # assignments accessible from users
+    resources :users do
+      resources :assignments
+    end
+
+    # assignments
     resources :assignments
+
+    # tenants
+    resources :tenants
   end
 
+  post 'auth' => 'auth#authenticate'
+  
 end
