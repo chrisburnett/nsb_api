40640e532bb71863cc1e6f6c918588c6f26488a7
Chris Burnett <dameramu@gmail.com>
2017-06-15 18:03:59 +0100

    Update test cases for GET auth

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe 'Auth API', type: :request do
   # initialize test data 
   let!(:user) { create(:user) }
 
-  describe 'POST /api/v1/auth' do
+  describe 'GET /api/v1/auth' do
     it 'returns responds 200 to successful authentication requests' do
-      post '/api/v1/auth', headers: { HTTP_AUTHORIZATION: ActionController::HttpAuthentication::Basic.encode_credentials(user.username, user.password) }
+      get '/api/v1/auth', headers: { HTTP_AUTHORIZATION: ActionController::HttpAuthentication::Basic.encode_credentials(user.username, user.password) }
       expect(response).to have_http_status(200)
     end
     
     it 'returns responds 401 to unsuccessful authentication requests' do
-      post '/api/v1/auth', headers: { HTTP_AUTHORIZATION: ActionController::HttpAuthentication::Basic.encode_credentials(user.username, "wrongpass") }
+      get '/api/v1/auth', headers: { HTTP_AUTHORIZATION: ActionController::HttpAuthentication::Basic.encode_credentials(user.username, "wrongpass") }
       expect(response).to have_http_status(401)
     end
   end
