01e5da49d7e1995ab6944383650778f9e30d7475
Chris Burnett <dameramu@gmail.com>
2017-06-23 17:03:07 +0100

    Job.assigned now not persisted

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -8,7 +8,6 @@ class Assignment < ApplicationRecord
   validates_presence_of :job_id
   validates :status, inclusion: { in: %w(pending accepted rejected cancelled).push(nil) }
   before_create :set_assignment_date
-  before_save :update_job_status
 
   # active assignments are the latest accepted assignments on open jobs
   scope :active, -> { includes(:job)
@@ -27,16 +26,8 @@ class Assignment < ApplicationRecord
   def set_assignment_date
     self.assignment_date = Time.now
   end
-
-  def update_job_status
-    # if the current most recent task changes state, update job to reflect
-    # rejecting the current assignment returns job to unassigned status
-    if id == job.try(:latest_assignment).try(:id) && status_changed? then
-      if status == "accepted" then
-        job.update_attribute(:assigned, true)
-      elsif status == "rejected" || status == nil then
-        job.update_attribute(:assigned, false)
-      end
-    end
+  def set_response_date
+    self.response_date = Time.now
   end
+
 end
