f237c7748f6ca7e628db1650dd44a3dd12b7f603
Chris Burnett <dameramu@gmail.com>
2017-06-26 13:38:47 +0100

    Datatables working w/ bootstrap look

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,8 @@
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require turbolinks
 //= require bootstrap
 //= require jquery.slimscroll.min
-//= require dataTables/jquery.dataTables
 //= require_tree .
