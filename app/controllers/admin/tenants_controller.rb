6af5d56f020ce08255df55cfad2f145f95a1cc79
Chris Burnett <dameramu@gmail.com>
2017-06-28 03:23:54 +0100

    Search across name and address

--- app/controllers/admin/tenants_controller.rb
+++ app/controllers/admin/tenants_controller.rb
@@ -6,7 +6,7 @@ class Admin::TenantsController < SecureAdminController
     # if a search term is provided, then return matching id/tenant
     # pairs for autocomplete fields
     if params[:term] then
-      render json: Tenant.where("name ILIKE ?", "%#{params[:term]}%").map { |t| {id: t.id, value: t.name} }
+      render json: Tenant.where("name || ' ' || address ILIKE ?", "%#{params[:term]}%").map { |t| {id: t.id, value: "#{t.name}, #{t.address}" } }
     else
       render json: Tenant.all.to_json
     end
