817ea6436cc62277f1d1171b6efbe2030f8c2372
Chris Burnett <dameramu@gmail.com>
2017-08-20 22:47:58 +0100

    Fix broken migration

--- db/migrate/20170816144215_rename_short_title_to_job_number.rb
+++ db/migrate/20170816144215_rename_short_title_to_job_number.rb
@@ -1,5 +1,5 @@
 class RenameShortTitleToJobNumber < ActiveRecord::Migration[5.1]
   def change
-    rename_column :jobs, :job_number, :job_number
+    rename_column :jobs, :short_title, :job_number
   end
 end
