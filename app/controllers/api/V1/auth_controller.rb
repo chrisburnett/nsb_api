cb8801a1e14d79902546b24fbce8f4cfca2fe906
Chris Burnett <dameramu@gmail.com>
2017-09-08 10:37:36 +0100

    Allow passing of FCM token at auth

--- app/controllers/api/V1/auth_controller.rb
+++ app/controllers/api/V1/auth_controller.rb
@@ -7,11 +7,13 @@ class Api::V1::AuthController < ApplicationController
     authenticate_or_request_with_http_basic do |username, password| 
       user = User.find_by_username(username).try(:authenticate, password)
       if user
+        if params[:registration_id] then
+          user.update(registration_id: params[:registration_id])
+        end
         render json: { auth_token: user.generate_auth_token, username: user.username, fullname: user.name }, status: :ok
       else
         render json: { error: 'Invalid username or password' }, status: :unauthorized
       end
     end
   end
-
 end
