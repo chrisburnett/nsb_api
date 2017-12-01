b7d1c5d70b4a8c82714143d94afc3c3cdeaaa297
Chris Burnett <dameramu@gmail.com>
2017-12-01 01:02:34 +0000

    Fix upload paths for S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
   else
     config.fog_provider = 'fog/aws'
     config.fog_use_ssl_for_aws = true
-    config.fog_directory  = 'nsb-storage/attachments'
+    config.fog_directory  = 'nsb-storage'
     config.fog_public     = false
     config.fog_attributes = { 'Cache-Control': 'max-age=315576000' }
     config.asset_host = 'https://nsb-storage.s3.amazonaws.com'
