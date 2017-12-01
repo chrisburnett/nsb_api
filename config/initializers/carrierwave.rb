347778d13fdf7e754466d899f336569e1d1d1e85
Chris Burnett <dameramu@gmail.com>
2017-12-01 00:45:27 +0000

    Use correct AWS endpoint

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'nsb-storage/attachments'
     config.fog_public     = false
     config.fog_attributes = { 'Cache-Control': 'max-age=315576000' }
-    config.asset_host = 'https://s3.amazonaws.com/nsb-storage'
+    config.asset_host = 'https://nsb-storage.s3.amazonaws.com'
 
     config.fog_credentials = {
       provider:               'AWS',
